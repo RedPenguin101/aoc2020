f892442468daecd892da26457f46ea00ea902bdd
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-12-13 16:35:17 +0000

    day13 part 2

--- day13.clj
+++ day13.clj
@@ -8,3 +8,20 @@
   (apply * (first (sort-by second
                            (for [id ids]
                              [id (- (* id (int (Math/ceil (/ ts id)))) ts)])))))
+
+(defn- linear-congruence-solve [pairs]
+  (let [remainders (map first pairs)
+        modulos (map second pairs)
+        prod (apply * modulos)
+        pp (map #(/ prod %) modulos)
+        inv (map #(.modInverse (biginteger %1) (biginteger %2)) pp modulos)]
+    (mod (apply + (map * remainders pp inv)) prod)))
+
+(def input (keep-indexed
+            (fn [i n] (when n
+                        [(bigint (- i)) n]))
+            (map #(try (bigint (Integer/parseInt %))
+                       (catch Exception e nil))
+                 (str/split (second (str/split-lines (slurp "resources/day13input"))) #","))))
+
+(linear-congruence-solve input)
