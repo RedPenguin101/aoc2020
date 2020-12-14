ccf987204e320631172aa8322b793fe16d9d1f42
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-12-14 14:27:46 +0000

    day14 part 2

--- day14.clj
+++ day14.clj
@@ -33,3 +33,42 @@
      :memory
      vals
      (apply +))
+
+(defn- apply-mask-v2 [mask bin-array]
+  (let [i (volatile! -1)]
+    (map #(case %1
+            0 %2
+            1 1
+            nil (do
+                  (vswap! i inc)
+                  (vector :r @i)))
+         mask bin-array)))
+
+(defn- memory-addresses [result]
+  (let [replacements (filter coll? result)
+        n (count replacements)]
+    (map (comp #(Long/parseLong % 2) #(apply str %))
+         (map #(replace % result)
+              (map #(zipmap replacements %) (combo/permuted-combinations (apply concat [(repeat n 0) (repeat n 1)]) n))))))
+
+(defn- mem-push [mem locs val]
+  (reduce (fn [m loc]
+            (assoc m loc val))
+          mem
+          locs))
+
+(defn- update-memory-v2 [{:keys [mask memory] :as state} line]
+  (if (= "mem" (subs line 0 3))
+    (let [[mem-loc number] (parse-instruction line)]
+      (assoc state :memory (mem-push memory
+                                     (memory-addresses (apply-mask-v2 mask (binary-array mem-loc 36)))
+                                     number)))
+    (assoc state :mask (parse-mask line))))
+
+(->> "resources/day14input"
+     slurp
+     str/split-lines
+     (reduce update-memory-v2 {})
+     :memory
+     vals
+     (apply +))
