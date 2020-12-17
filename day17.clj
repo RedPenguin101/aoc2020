14b9c8e842efae22c0b4cdccc270a01ec0912b9d
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-12-17 16:01:21 +0000

    day17 part 2

--- day17.clj
+++ day17.clj
@@ -4,6 +4,9 @@
 (defn- parse-input [input]
   (remove nil? (apply concat (map-indexed (fn bleh [idx row] (map-indexed #(if (= \# %2) [idx %1 0] nil) row)) (str/split-lines input)))))
 
+(defn- parse-input-4d [input]
+  (remove nil? (apply concat (map-indexed (fn bleh [idx row] (map-indexed #(if (= \# %2) [idx %1 0 0] nil) row)) (str/split-lines input)))))
+
 (defn- neighbors [[x y z]]
   (for [xvar [-1 0 1]
         yvar [-1 0 1]
@@ -11,30 +14,28 @@
         :when  (not= xvar yvar zvar 0)]
     [(+ x xvar) (+ y yvar) (+ z zvar)]))
 
-(defn- step [coords]
+(defn- neighbors-4d [[x y z w]]
+  (for [xvar [-1 0 1]
+        yvar [-1 0 1]
+        zvar [-1 0 1]
+        wvar [-1 0 1]
+        :when  (not= xvar yvar zvar wvar 0)]
+    [(+ x xvar) (+ y yvar) (+ z zvar) (+ w wvar)]))
+
+(defn- step [neighbors coords]
   (let [stuff (reduce (fn [A [c freq]] (if (#{2 3} freq) (update A freq conj c) A)) {} (frequencies (apply concat (map neighbors coords))))]
     (concat (get stuff 3)
             (filter (set coords) (get stuff 2)))))
 
-(defn- print-layer [[z coords] [xmin xmax ymin ymax]]
-  (str "z=" z "\n"
-       (let [coords (set (map #(take 2 %) coords))]
-         (str/join "\n"
-                   (map str/join
-                        (partition (- (inc xmax) xmin)
-                                   (for [x (range xmin (inc xmax))
-                                         y (range ymin (inc ymax))]
-                                     (if (coords [x y]) "#" "."))))))
-       "\n\n"))
-
-(defn- print-grid [coords]
-  (let [x-max (apply max (map first coords))
-        x-min (apply min (map first coords))
-        y-max (apply max (map second coords))
-        y-min (apply min (map second coords))]
-    (for [layer (sort (group-by #(nth % 2) (sort coords)))]
-      (print-layer layer [x-min x-max y-min y-max]))))
-
 (comment
-  (println (str/join (print-grid (last (take 7 (iterate step (parse-input (slurp "resources/day17input"))))))))
-  (count (last (take 7 (iterate step (parse-input (slurp "resources/day17input")))))))
+  (time (count (last (take 7 (iterate (partial step neighbors) (parse-input (slurp "resources/day17example")))))))
+  ;; => 112
+  (time (count (last (take 7 (iterate (partial step neighbors) (parse-input (slurp "resources/day17input")))))))
+  ;; => 353
+
+
+  (count (last (take 7 (iterate (partial step neighbors-4d) (parse-input-4d (slurp "resources/day17example"))))))
+  ;; => 848
+  (time (count (last (take 7 (iterate (partial step neighbors-4d) (parse-input-4d (slurp "resources/day17input")))))))
+  ;; => 2472
+)
