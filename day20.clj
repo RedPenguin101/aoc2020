976c52480a025a66060aa1a8ed2f50d9c0d90bf8
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-12-20 16:25:29 +0000

    add tranform applied to structure

--- day20.clj
+++ day20.clj
@@ -34,9 +34,14 @@
 ;; => 17250897231301
 
 (defn- xforms [[a b c d]]
-  (vector [a b c d] [d a b c] [c d a b] [b c d a] 
-          [c b a d] [a d c b]
-          [b a d c]))
+  (vector [[a b c d] :none]
+          [[d a b c] :r1]
+          [[c d a b] :r2]
+          [[b c d a] :r3]
+          [[c b a d] :fv]
+          [[a d c b] :fh]
+          [[b a d c] :fd1]
+          [[d c b a] :fd2]))
 
 (defn- position-of [x ys]
   (first (keep-indexed (fn [idx val] (when (= x val) idx)) ys)))
@@ -53,8 +58,10 @@
          true)))
 
 (defn- find-fit [a [id2 conns2]]
-  (some #(when (fit? a %) %)
-        (map #(vector id2 %) (xforms conns2))))
+  (first (filter #(fit? a %) (map (partial apply vector id2) (xforms conns2)))))
+
+(comment 
+  (find-fit [2729 [1427 1951 nil 2971] :r3] [1951 [2729 2311 nil nil]]))
 
 (defn- follow [direction this ys]
   (cons this
@@ -64,6 +71,11 @@
                    (find-fit this [next-id (ys next-id)]))
                  ys))))
 
+(comment 
+  ((border-matches ex-tiles) 1951)
+
+  (take-while seq (follow second [2971 [1489 2729 nil nil] :r3] (into (sorted-map) (border-matches ex-tiles)))))
+
 (defn get-picture [start tiles]
   (let [ys (into (sorted-map) (border-matches tiles))]
     (reverse (for [row-header (take-while seq (follow first start ys))]
@@ -72,3 +84,4 @@
 (def input-border-matches (border-matches input))
 
 (map #(map first %) (get-picture [1321 '(3761 2293 nil nil)] input))
+(get-picture [1321 '(3761 2293 nil nil)] input)
